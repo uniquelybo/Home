ae0a758d7e3ecee7d426a953a012cef82c31eaae
renxiaobo <renxiaobo@163.com>
2020-11-02 11:36:08 +0800

    delete api_test

--- api_test/test.py
+++
@@ -1,7 +0,0 @@
-from api_test.Read_API_Data import Read_API_Data
-import datetime
-
-if __name__ == "__main__":
-    print(datetime.datetime.now())
-    read_api = Read_API_Data()
-    read_api.execute_api()
