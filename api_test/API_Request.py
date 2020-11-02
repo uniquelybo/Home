996d3887bc28a695209afa8b689a97411c865cc8
renxiaobo <renxiaobo@163.com>
2020-11-02 11:15:29 +0800

    commit API_TEST

---
+++ api_test/API_Request.py
@@ -0,0 +1,84 @@
+import requests
+import pandas as pd
+import json
+
+
+class API_Request():
+    def __init__(self, api_data, token):
+        self.url = api_data['url']
+        self.method = api_data['method']
+        self.data = api_data['data']
+        self.json_data = api_data['json']
+        self.headers = api_data['headers']
+        self.token = token
+
+    def request(self):
+        if self.method == "POST":
+            if pd.isnull(self.headers):
+                if pd.isnull(self.data):
+                    if pd.isnull(self.json_data):
+                        response = requests.post(url=self.url)
+                    else:
+                        self.json_data = json.loads(self.json_data)
+                        response = requests.post(url=self.url, json=self.json_data)
+                else:
+                    if pd.isnull(self.json_data):
+                        self.data = json.loads(self.data)
+                        response = requests.post(url=self.url, data=self.data)
+                    else:
+                        self.data = json.loads(self.data)
+                        self.json_data = json.loads(self.json_data)
+                        response = requests.post(url=self.url, data=self.data, json=self.json_data)
+            else:
+                self.headers = json.loads(self.headers)
+                self.headers["token"] = self.token
+                if pd.isnull(self.data):
+                    if pd.isnull(self.json_data):
+                        response = requests.post(url=self.url, headers=self.headers)
+                    else:
+                        self.json_data = json.loads(self.json_data)
+                        response = requests.post(url=self.url, json=self.json_data, headers=self.headers)
+                else:
+                    if pd.isnull(self.json_data):
+                        self.data = json.loads(self.data)
+                        response = requests.post(url=self.url, data=self.data, headers=self.headers)
+                    else:
+                        self.data = json.loads(self.data)
+                        self.json_data = json.loads(self.json_data)
+                        response = requests.post(url=self.url, data=self.data, json=self.json_data, headers=self.headers)
+
+        elif self.method == "GET":
+            if pd.isnull(self.headers):
+                if pd.isnull(self.data):
+                    if pd.isnull(self.json_data):
+                        response = requests.get(url=self.url)
+                    else:
+                        self.json_data = json.loads(self.json_data)
+                        response = requests.get(url=self.url, json=self.json_data)
+                else:
+                    if pd.isnull(self.json_data):
+                        self.data = json.loads(self.data)
+                        response = requests.get(url=self.url, params=self.data)
+                    else:
+                        self.data = json.loads(self.data)
+                        self.json_data = json.loads(self.json_data)
+                        response = requests.get(url=self.url, params=self.data, json=self.json_data)
+            else:
+                self.headers = json.loads(self.headers)
+                self.headers["token"] = self.token
+                if pd.isnull(self.data):
+                    if pd.isnull(self.json_data):
+                        response = requests.get(url=self.url, headers=self.headers)
+                    else:
+                        self.json_data = json.loads(self.json_data)
+                        response = requests.get(url=self.url, json=self.json_data, headers=self.headers)
+                else:
+                    if pd.isnull(self.json_data):
+                        self.data = json.loads(self.data)
+                        response = requests.get(url=self.url, params=self.data, headers=self.headers)
+                    else:
+                        self.data = json.loads(self.data)
+                        self.json_data = json.loads(self.json_data)
+                        response = requests.get(url=self.url, params=self.data, json=self.json_data,
+                                                 headers=self.headers)
+        return response
---
+++ api_test/Request_Get_Token.py
@@ -0,0 +1,18 @@
+import requests
+import sys
+
+
+def get_token():
+    print("获取token.........")
+    try:
+        data = {"userid": "344267", "uttnum": "10238260", "uphone": "17363905237", "jiguangid": "13065ffa4eb9904bcf2", "facility": "0"}
+        login_url = "http://192.168.16.226:8586/app/user/add"
+        response = requests.post(url=login_url, data=data)
+        # print(response.text)
+        token = response.json()["user"]["token"]
+        print(token)
+        return token
+    except Exception as ex:
+        print("token获取失败。。。。", ex)
+        sys.exit()
+
